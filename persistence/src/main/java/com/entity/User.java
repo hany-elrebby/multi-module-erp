dac93d862d897ca3544859d89e786afd3fe45371
hanyelrebby <hanyelrebby0125@gmail.com>
2025-11-07 22:29:51 +0200

    added User entity

---
+++ persistence/src/main/java/com/entity/User.java
@@ -0,0 +1,25 @@
+package com.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+}
